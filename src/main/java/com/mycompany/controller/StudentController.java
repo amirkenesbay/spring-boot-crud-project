8d8cf4e32f407de44adde48c1fa41b11f1ca3e04
amirkenesbay <amirkenesbay@gmail.com>
2022-03-04 04:06:20 +0600

    Add student edit form

--- src/main/java/com/mycompany/controller/StudentController.java
+++ src/main/java/com/mycompany/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -26,5 +28,12 @@ public class StudentController {
         model.addAttribute("student", new Student());
         model.addAttribute("pageTitle", "Add new Student");
         return "student_form";
+    }
+
+    @PostMapping("/students/save")
+    public String saveStudent(Student student, RedirectAttributes ra){
+        service.save(student);
+        ra.addFlashAttribute("message", "The student has been saved successfully");
+        return "redirect:/students";
     }
 }
